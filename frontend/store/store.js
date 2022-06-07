245afc97f24e36c300fb5ef52d274554669fcd2b
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-06-07 16:45:46 -0400

    we got state

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
-import { configureStore } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from "../reducers/root_reducer";
 
 const configStore = (preloadedState = {}) => {
-    return configureStore(rootReducer, preloadedState)
+    return createStore(rootReducer, preloadedState)
 }
 export default configStore;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,6 +1,16 @@
 import { receiveTodos, receiveTodo } from  "./actions/todo_actions"
 import todosReducer from "./reducers/todos_reducer";
 import React from 'react';
-import store from "./store/store"
+import ReactDOM from "react-dom"
+import configStore from "./store/store"
 
-window.store = store
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root');
+    const store = configStore()
+    window.store = store
+
+    ReactDOM.render(<h1>Todos App</h1>, root);
+
+})
